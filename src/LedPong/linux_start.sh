3e24dec879199945494e4abf3c90d3cff2964b3c
Michael Hoffmeister <michahofft2@googlemail.com>
2026-05-24 13:34:55 +0200

    1st update after break

--- src/LedPong/linux_start.sh
+++ src/LedPong/linux_start.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-echo "Needs tp run as sudo!"
+echo "Needs to run as sudo!"
 chmod 666 /dev/ttyACM0
 stty -F /dev/ttyACM0 115200
-./program.bin --led-multiplexing=0 --led-pwm-bits=11 --led-gpio-mapping=adafruit-hat --led-pixel-mapper="PiPong" -swap
+# ./program.bin --led-multiplexing=0 --led-pwm-bits=11 --led-gpio-mapping=adafruit-hat --led-pixel-mapper="PiPong" -swap
+SDL_AUDIODRIVER=alsa AUDIODEV=hw:2,0 ./program.bin --led-multiplexing=0 --led-pwm-bits=11 --led-gpio-mapping=adafruit-hat --led-pixel-mapper="PiPong" -swap
